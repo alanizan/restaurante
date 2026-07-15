94b5edc5a4dae7df2baba5b09f1b5033aaa3b3a6
m4pro <anthonyalabz1@gmail.com>
2026-07-15 11:19:11 -0600

    creacion de menu

--- RestauranteMesaReal/src/restaurantemesareal/Principal.java
+++ RestauranteMesaReal/src/restaurantemesareal/Principal.java
@@ -1,9 +1,12 @@
 package restaurantemesareal;
 
 public class Principal {
-    
+
     public static void main(String[] args) {
-        
+
+        Restaurante r = new Restaurante();
+        r.menu();
+
     }
-    
+
 }
